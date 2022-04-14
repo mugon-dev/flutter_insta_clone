5117f19579541ac10d7d15dc006916df30433c2b
jmg <jmg41490400@gmail.com>
2022-04-14 15:04:16 +0900

    Initial commit, bottom navigation, manage history

---
+++ lib/src/app.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:insta_clone/src/components/image_data.dart';
+import 'package:insta_clone/src/controller/bottom_nav_controller.dart';
+
+class App extends GetView<BottomNavController> {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        child: Obx(
+          () => Scaffold(
+            appBar: AppBar(),
+            body: IndexedStack(
+              index: controller.pageIndex.value,
+              children: [
+                Container(child: Text('Home')),
+                Container(child: Text('search')),
+                Container(child: Text('upload')),
+                Container(child: Text('activity')),
+                Container(child: Text('mypage')),
+              ],
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed, // active시 올라가는 애니메이션 제거
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              elevation: 0,
+              currentIndex: controller.pageIndex.value,
+              onTap: controller.changeBottomNav,
+              items: [
+                BottomNavigationBarItem(icon: ImageData(IconsPath.homeOff), activeIcon: ImageData(IconsPath.homeOn), label: 'home'),
+                BottomNavigationBarItem(icon: ImageData(IconsPath.searchOff), activeIcon: ImageData(IconsPath.searchOn), label: 'home'),
+                BottomNavigationBarItem(icon: ImageData(IconsPath.uploadIcon), label: 'home'),
+                BottomNavigationBarItem(icon: ImageData(IconsPath.activeOff), activeIcon: ImageData(IconsPath.activeOn), label: 'home'),
+                BottomNavigationBarItem(
+                    icon: Container(
+                      decoration: const BoxDecoration(shape: BoxShape.circle, color: Colors.grey),
+                      width: 30,
+                      height: 30,
+                    ),
+                    label: 'home'),
+              ],
+            ),
+          ),
+        ),
+        onWillPop: controller.willPopAction);
+  }
+}
---
+++ lib/src/controller/bottom_nav_controller.dart
@@ -0,0 +1,65 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:insta_clone/src/components/message_popup.dart';
+import 'package:insta_clone/src/pages/upload.dart';
+
+enum PageName { HOME, SEARCH, UPLOAD, ACTIVITY, MYPAGE }
+
+class BottomNavController extends GetxController {
+  RxInt pageIndex = 0.obs;
+  List<int> bottomHistory = [0];
+  void changeBottomNav(int value, {bool hasGesture = true}) {
+    var page = PageName.values[value];
+    switch (page) {
+      case PageName.UPLOAD:
+        Get.to(() => const Upload());
+        break;
+      case PageName.HOME:
+      case PageName.SEARCH:
+      case PageName.ACTIVITY:
+      case PageName.MYPAGE:
+        _changePage(value, hasGesture: hasGesture);
+        break;
+    }
+  }
+
+  void _changePage(int value, {bool hasGesture = true}) {
+    pageIndex(value);
+    if (!hasGesture) return;
+
+    ///history 누적 방식
+    if (bottomHistory.last != value) {
+      bottomHistory.add(value);
+    }
+
+    /// 누적 x 중복 x
+    // if (bottomHistory.contains(value)) {
+    //   bottomHistory.remove(value);
+    // }
+    // bottomHistory.add(value);
+  }
+
+  Future<bool> willPopAction() async {
+    /// 쌓인게 없을때
+    if (bottomHistory.length == 1) {
+      showDialog(
+          context: Get.context!,
+          builder: (context) => MessagePopup(
+                title: '시스템',
+                message: '종료하시겠습니까?',
+                okCallback: () {
+                  exit(0);
+                },
+                cancelCallback: Get.back,
+              ));
+      return true;
+    } else {
+      bottomHistory.removeLast();
+      var index = bottomHistory.last;
+      changeBottomNav(index, hasGesture: false);
+      return false;
+    }
+  }
+}
